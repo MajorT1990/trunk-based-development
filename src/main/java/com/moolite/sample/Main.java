9492af389289aab2f405799e9eee56a54471603d
Wouter <wouter.moermans@gmail.com>
2018-09-22 23:47:15 +0200

    Initial commit!

---
+++ src/main/java/com/moolite/sample/Main.java
@@ -0,0 +1,7 @@
+package com.moolite.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Dancing in the moolite, everybody's feeling warm and bright~~");
+    }
+}
